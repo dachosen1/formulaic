d2913a57e67c9649ef6b17fb07dbe2832cfef6f8
decaff46 <cl3802@columbia.edu>
2019-05-30 14:39:01 -0400

    most updated version - still need to set up environmental variables though

--- R/create.formula.R
+++ R/create.formula.R
@@ -25,6 +25,8 @@ add.backtick <- function(x, include.backtick = "as.needed"){
 #' @param input.names The names of the variables with the full names delineated.
 #' @param input.patterns Includes additional input variables.  The user may enter patterns -- e.g. to include every variable with a name that includes the pattern.  Multiple patterns may be included as a character vector.  However, each pattern may not contain spaces and is otherwise subject to the same limits on patterns as used in the grep function.
 #' @param dat User can specify a data.frame object that will be used to remove any variables that are not listed in names(dat. As default it is set as NA. In this case, the formula is created simply from the outcome.name and input.names.
+#' @param interactions This is meant to be a list of character vectors. Each entry of the list is meant to specify all of the terms that will form a single interaction.
+#' @param force.main.effects This is a logical value.  When TRUE, the intent is that any term included as an interaction (of multiple variables) must also be listed individually as a main effect.
 #' @param reduce This is an option that user can take. As default it is set as FALSE. When the user set it as TRUE, it will go through the logic of checking for too few contrasts or too many contrasts.
 #' @param max.input.categories This limits the maximum number of variables that will be employeed in the formula. As default it is set at 20, but users can still change at his/her convinence.
 #' @param max.outcome.categories.to.search This limits the maximum number of outcome categories will be investigated in the formula. As default it is set at 20, but users can still change at his/her convinence.
@@ -46,12 +48,11 @@ add.backtick <- function(x, include.backtick = "as.needed"){
 #'  create.formula(outcome.name = "y", input.names = "x", input.patterns = c("pi", "xel"), dat = dd)
 #' @import stats
 #' @export
-create.formula <- function(outcome.name, input.names, input.patterns = NA, dat = NA, reduce = FALSE, max.input.categories = 20, max.outcome.categories.to.search = 4, order.as = "as.specified", include.backtick = "as.needed", format.as = "formula"){
+create.formula <- function(outcome.name, input.names = NA, input.patterns = NA, dat = NA, interactions = NA, force.main.effects = TRUE, reduce = FALSE, max.input.categories = 20, max.outcome.categories.to.search = 4, order.as = "as.specified", include.backtick = "as.needed", format.as = "formula"){
 
 
   if(is.data.frame(dat)){
 
-    #require(data.table)
     data.table::setDT(dat)
 
     if(length(names(dat)) == 0){
@@ -73,10 +74,26 @@ create.formula <- function(outcome.name, input.names, input.patterns = NA, dat =
       variable.names.from.patterns <- names(dat)[grep(pattern = pattern, x = names(dat))]
     }
 
-    inclusion.table <- data.table(variable = unique(c(input.names, variable.names.from.patterns)))
+    if(!is.na(interactions[1])) {
+      unlisted.interactions <- unlist(interactions)
+    } else {
+      unlisted.interactions <- NULL
+    }
+
+    inclusion.table <- data.table(variable = unique(c(input.names, variable.names.from.patterns, unlisted.interactions)))[!is.na(variable)]
+
+    num.from.input.names <- length(input.names[!is.na(input.names)])
+
+    num.from.input.patterns <- length(variable.names.from.patterns[!(variable.names.from.patterns %in% input.names)])
+
+    num.from.interactions <- length(unique(unlisted.interactions[!(unlisted.interactions %in% c(input.names, variable.names.from.patterns))]))
+
     inclusion.table[variable %in% names(dat), class := as.character(dat[, as.character(lapply(X = .SD, FUN = "class")), .SDcols = variable]), by = variable]
     inclusion.table[, order := 1:.N]
-    inclusion.table[, specified.from := c(rep.int(x = "input.names", times = length(input.names)), rep.int(x = "input.patterns", times = .N - length(input.names)))]
+    ###
+    inclusion.table[, specified.from := c(rep.int(x = "input.names", times = num.from.input.names), rep.int(x = "input.patterns", times = num.from.input.patterns), rep.int(x = "interactions", times = num.from.interactions))]
+    ###
+
     inclusion.table[, exclude.not.in.names.dat := !(variable %in% names(dat))]
     inclusion.table[, exclude.matches.outcome.name := (variable == outcome.name)]
 
@@ -95,7 +112,7 @@ create.formula <- function(outcome.name, input.names, input.patterns = NA, dat =
 
       min.categories.tab[, exclude.lack.contrast := min.categories < 2]
 
-      inclusion.table <- merge(x = inclusion.table, y = min.categories.tab, by = "variable")
+      inclusion.table <- merge(x = inclusion.table, y = min.categories.tab, by = "variable", all.x = TRUE, all.y = TRUE)
 
       inclusion.table[, exclude.numerous.categories := min.categories > max.input.categories & class %in% c("character", "factor")]
 
@@ -105,21 +122,53 @@ create.formula <- function(outcome.name, input.names, input.patterns = NA, dat =
 
     exclusion.columns <- grep(pattern = "exclude", x = names(inclusion.table))
 
-    inclusion.table[, include.variable := rowMeans(.SD) == 0, .SDcols = exclusion.columns]
+    inclusion.table[, include.variable := rowMeans(.SD, na.rm = TRUE) == 0, .SDcols = exclusion.columns]
 
-
-    all.input.names <- inclusion.table[include.variable == TRUE, variable]
+    if(force.main.effects == TRUE){
+      all.input.names <- inclusion.table[include.variable == TRUE, variable]
+    }
+    if(force.main.effects == FALSE){
+      all.input.names <- inclusion.table[include.variable == TRUE & specified.from != "interactions", variable]
+    }
 
     if(order.as == "column.order"){
       all.input.names <- names(dat)[names(dat) %in% all.input.names]
     }
-  }
-  if(!is.data.frame(x = dat)){
-    all.input.names <- input.names
-    inclusion.table <- "dat was not provided (NA); no inclusion table was computed."
+
+
+    # Compute included.interactions
+
+    include.interaction <- as.logical(lapply(X = interactions, FUN = function(x){return(inclusion.table[variable %in% x, min(include.variable) == 1])}))
+
+    interactions.with.backtick <- lapply(X = interactions, FUN = "add.backtick", include.backtick = include.backtick)
+
+    all.interaction.terms <- as.character(lapply(X = interactions.with.backtick, FUN = "paste", collapse = " * "))
+
+    interaction.terms <- all.interaction.terms[include.interaction == TRUE]
+
+    interactions.table <- data.table(interactions = all.interaction.terms, include.interaction = include.interaction)
+
   }
 
-  if(length(all.input.names) == 0){
+  if(!is.data.frame(x = dat)){
+
+    if(!is.na(interactions[[1]])){
+      interactions.with.backtick <- lapply(X = interactions, FUN = "add.backtick", include.backtick = include.backtick)
+
+      interaction.terms <- as.character(lapply(X = interactions.with.backtick, FUN = "paste", collapse = " * "))
+    }
+    if(is.na(interactions[[1]])){
+      interaction.terms <- NA
+    }
+
+    all.input.names <- input.names
+    inclusion.table <- data.table()
+    interactions.table <- data.table()
+    inclusion.table.statement <- "dat was not provided (NA); no inclusion table was computed."
+    interactions.table.statement <- "dat was not provided (NA); no interactions.table object was computed."
+  }
+
+  if(length(c(all.input.names[!is.na(all.input.names)], interaction.terms[!is.na(interaction.terms)])) == 0){
     all.input.names <- "1"
   }
 
@@ -132,13 +181,21 @@ create.formula <- function(outcome.name, input.names, input.patterns = NA, dat =
 
   input.names.delineated <- add.backtick(x =  all.input.names, include.backtick = include.backtick)
   outcome.name.delineated <- add.backtick(x = outcome.name, include.backtick = include.backtick)
-  the.formula <- sprintf("%s ~ %s", outcome.name.delineated, paste(input.names.delineated, collapse = "+"))
+
+  rhs.with.missing <- c(input.names.delineated, interaction.terms)
+  rhs <- rhs.with.missing[!is.na(rhs.with.missing)]
+
+  if(length(rhs) == 0){
+    rhs <- "1"
+  }
+
+  the.formula <- sprintf("%s ~ %s", outcome.name.delineated, paste(rhs, collapse = "+"))
 
   if(format.as == "formula"){
     the.formula <- stats::as.formula(the.formula)
   }
 
-  res <- list(formula = the.formula, inclusion.table = inclusion.table)
+  res <- list(formula = the.formula, inclusion.table = inclusion.table, interactions.table = interactions.table)
 
   return(res)
 }
@@ -151,12 +208,13 @@ create.formula <- function(outcome.name, input.names, input.patterns = NA, dat =
 #' @param  dat Data frame, list or environment (or object coercible by as.data.frame to a data frame) containing the variables in the model.
 #' @param  max.input.categories This limits the maximum number of variables that will be employeed in the formula. As default it is set at 20, but users can still change at his/her convinence.
 #' @param  max.outcome.categories.to.search This limits the maximum number of outcome categories will be investigated in the formula. As default it is set at 20, but users can still change at his/her convinence.
+#' @param  force.main.effects This is a logical value.  When TRUE, the intent is that any term included as an interaction (of multiple variables) must also be listed individually as a main effect.
 #' @param  order.as  rearranges its first argument into ascending or descending order.
 #' @param  include.backtick Add backticks to make a appropriate variable
 #' @param  format.as The data type of the output.  If not set as "formula", then a character vector will be returned.
 #'
 #' @export
-reduce.existing.formula <- function(the.initial.formula, dat, max.input.categories = 20, max.outcome.categories.to.search = 4, order.as = "as.specified", include.backtick = "as.needed", format.as = "formula"){
+reduce.existing.formula <- function(the.initial.formula, dat, max.input.categories = 20, max.outcome.categories.to.search = 4, force.main.effects = TRUE, order.as = "as.specified", include.backtick = "as.needed", format.as = "formula"){
 
   if(class(the.initial.formula) == "formula"){
     the.sides <- as.character(the.initial.formula)[2:3]
@@ -169,7 +227,28 @@ reduce.existing.formula <- function(the.initial.formula, dat, max.input.categori
 
   the.pieces.untrimmed <- strsplit(x = the.sides[2], split = "+", fixed = TRUE)[[1]]
   the.pieces.untrimmed.2 <- gsub(pattern = "`", replacement = "", x = the.pieces.untrimmed, fixed = TRUE)
-  input.names <- trimws(x = the.pieces.untrimmed.2, which = "both")
 
-  return(create.formula(outcome.name = outcome.name, input.names = input.names, input.patterns = NA, dat = dat, reduce = TRUE, max.input.categories = max.input.categories, max.outcome.categories.to.search = max.outcome.categories.to.search, order.as = order.as, include.backtick = include.backtick, format.as = format.as))
+  the.pieces.trimmed <- trimws(x = the.pieces.untrimmed.2, which = "both")
+
+  w.int <- grep(pattern = "*", x = the.pieces.trimmed)
+  w.input <- (1:length(the.pieces.trimmed))[!((1:length(the.pieces.trimmed)) %in% w.int)]
+
+  if(length(w.input) == 0){
+    input.names <- NA
+  }
+  if(length(w.input) > 0){
+    input.names <- the.pieces.trimmed[w.input]
+  }
+
+  if(length(w.int) == 0){
+    interactions <- NA
+  }
+  if(length(w.int) > 0){
+    untrimmed.interactions <- strsplit(x = the.pieces.trimmed[w.int], split = "*", fixed = TRUE)
+    interactions <- lapply(X = untrimmed.interactions, FUN = "trimws", which = "both")
+
+  }
+
+  return(create.formula(outcome.name = outcome.name, input.names = input.names, input.patterns = NA, dat = dat, interactions = interactions, force.main.effects = force.main.effects, reduce = TRUE, max.input.categories = max.input.categories, max.outcome.categories.to.search = max.outcome.categories.to.search, order.as = order.as, include.backtick = include.backtick, format.as = format.as))
 }
+

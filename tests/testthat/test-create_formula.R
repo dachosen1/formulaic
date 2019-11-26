b14afe53a119d3ed2306dd8455ada4ff86f12ac8
dachosen1 <anderson.nelson1@gmail.com>
2019-11-26 15:01:05 -0500

    Update Vignette and added a test tiles

---
+++ tests/testthat/test-create_formula.R
@@ -0,0 +1,41 @@
+context("Create Formula")
+
+n <- 10
+dd <-
+  data.table::data.table(w = rnorm(n = n),
+                         x = rnorm(n = n),
+                         pixel_1 = rnorm(n = n))
+dd[, 'pixel 2' := 0.3 * pixel_1 + rnorm(n)]
+dd[, pixel_3 := 0.3 * pixel_1 + rnorm(n)]
+dd[, item_1 := 0.3 * pixel_3 + rnorm(n)]
+dd[, item_2 := 0.3 * pixel_3 + rnorm(n)]
+dd[, y := 5 * x + 3 * pixel_1 + 2 * pixel_3 + rnorm(n)]
+
+formula.1 <- create.formula(
+  outcome.name = "y",
+  input.names = c("x", "Random error", "y"),
+  input.patterns = c("pix"),
+  dat = dd
+)
+
+
+formula.2 <- create.formula(
+  outcome.name = "y",
+  input.names = c("x", "Random error", "y"),
+  input.patterns = c("pix"),
+  dat = dd,
+  include.backtick = FALSE
+)
+
+test_that('Formula resutls',
+          {
+            expect_equal(formula.1$formula, y ~ x + pixel_1 + `pixel 2` + pixel_3)
+            expect_false(formula.1$inclusion.table[formula.1$inclusion.table$variable == "Random error"]$include.variable)
+          })
+
+
+
+
+
+
+
